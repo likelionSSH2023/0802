9c8974acd9b9cf713ba61be3a16d0bdd826716a3
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-08-09 21:33:51 +0900

    feat: fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/likelionSSH2023/0802/",
+  base: "/0802/",
   plugins: [react()],
 });
